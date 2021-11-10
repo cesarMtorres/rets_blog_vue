29fd85554c8e91915f698d883cfd7b4978b64d1b
Cesar Molina <cesar.molina.torres.05@gmail.com>
2021-11-10 14:13:59 -0500

    add debuggin in (urls), add rest_framework in (setting)

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -129,7 +129,11 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.AllowAny',
+            ]
+}
